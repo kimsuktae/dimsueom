6d6802ef4630310aaee6aa096f2122b686e6db7c
kimsuktae <kimsuktae@htbeyond.com>
2024-07-28 00:04:19 +0900

    리뷰 반영

--- src/main/kotlin/example/com/plugins/team/TeamService.kt
+++ src/main/kotlin/example/com/plugins/team/TeamService.kt
@@ -2,20 +2,17 @@ package example.com.plugins.team
 
 import example.com.plugins.member.Member
 import example.com.plugins.member.MemberService
-import kotlinx.coroutines.runBlocking
 
 class TeamService(
     private val memberService: MemberService,
 ) {
     private var team = listOf<Participant>()
 
-    fun getTeam(): List<Participant> {
-        runBlocking {
-            team = memberService.getAllMembers()
-                .filter { it.participate }
-                .map { it.toParticipate() }
-                .also { team = it }
-        }
+    suspend fun getTeam(): List<Participant> {
+        team = memberService.getAllMembers()
+            .filter { it.participate }
+            .map { it.toParticipate() }
+            .also { team = it }
 
         return team
     }
